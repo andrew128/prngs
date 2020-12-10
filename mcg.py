3672d3805d3111f3859547742951e0be490defe6
Andrew Wei <andrewwei98@gmail.com>
2020-12-10 13:01:43 -0800

    update parameters to continuous uniform sampling and rename variable in mcg

--- mcg.py
+++ mcg.py
@@ -11,23 +11,23 @@ def get_samples_using_mcg(prime, multiplier, seed, num_samples):
     
     previous_sample = seed
     for i in range(num_samples):
-        r_i = (multiplier * previous_sample) % prime
-        previous_sample = r_i
-        output.append(r_i)
+        next_sample = (multiplier * previous_sample) % prime
+        previous_sample = next_sample
+        output.append(next_sample)
 
     return output
 
-def get_uniform_continuous_samples(c, num_samples, prime=151,\
+def get_uniform_continuous_samples(a, b, num_samples, prime=151,\
                                     multiplier=7, seed=1):
     '''
     Get uniform continuous samples between 0 and c using
     a Multiplicative Congurential Generator.
     '''
     samples = get_samples_using_mcg(prime, multiplier, seed, num_samples)
-    uniform_samples = [(i/prime)*c for i in samples]
+    uniform_samples = [(i/prime)*(b-a) + a for i in samples]
     return uniform_samples
 
-def get_uniform_discrete_samples(c, num_samples, prime=151,\
+def get_uniform_discrete_samples(c, num_samples, prime=3719,\
                                     multiplier=7, seed=1):
     '''
     Get uniform discrete samples in [0, c) using
@@ -60,21 +60,21 @@ def get_arbitrary_discrete_samples(num_samples, p = [0.1, 0.3, 0.2, 0.4],\
 
 def main():
     # Uniform discrete
-    uniform_discrete_samples = get_uniform_discrete_samples(6, 10000)
+    uniform_discrete_samples = get_uniform_discrete_samples(6, 3000, prime=3719)
     plt.hist(uniform_discrete_samples, bins=6)
-    plt.title('10000 Uniform Discrete Samples Between 0 and 5')
+    plt.title('3000 Uniform Discrete Samples Between 0 and 5')
     plt.show()
 
     # Uniform continuous
-    uniform_continuous_samples = get_uniform_continuous_samples(2, 10000)
-    plt.hist(uniform_continuous_samples, bins=150)
-    plt.title('10000 Uniform Continuous Samples Between 0 and 2')
+    uniform_continuous_samples = get_uniform_continuous_samples(0, 2, 3000, prime=3719)
+    plt.hist(uniform_continuous_samples, bins='auto')
+    plt.title('3000 Uniform Continuous Samples Between 0 and 2')
     plt.show()
 
     # Arbitrary discrete
-    arbitrary_discrete_samples = get_arbitrary_discrete_samples(10000)
+    arbitrary_discrete_samples = get_arbitrary_discrete_samples(3000, prime=3719)
     plt.hist(arbitrary_discrete_samples, bins=4)
-    plt.title('10000 Arbitrary Discrete Samples')
+    plt.title('3000 Arbitrary Discrete Samples')
     plt.show()
 
 if __name__ == "__main__":
